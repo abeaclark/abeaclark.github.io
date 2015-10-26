2fd0312c0c589a1d17d31a8f71567f76a6714816
abe_clark <abeaclark@gmail.com>
2015-10-26 15:12:05 -0700

    remove button works

--- stock/add_sub.js
+++ stock/add_sub.js
@@ -8,8 +8,8 @@ $(document).ready(function () {
 
 
   $('#add').click(function() {
-    ticker = $("#ticker_input").val()
-    var html_to_append = '<tr class="row" id="' + ticker +  '_row"><td><button class="remove_button" id="' + ticker + '_remove">Remove</button></td><td id="' + ticker + '">' + ticker + '</td><td id="' + ticker + '_price">Current price</td><td>1-day</td></tr>';
+    ticker = $("#ticker_input").val().toUpperCase();
+    var html_to_append = '<tr class="row" id="' + ticker +  '_row"><td><button class="remove_button" id="' + ticker + '_remove">Remove</button></td><td class="ticker" id="' + ticker + '">' + ticker + '</td><td id="' + ticker + '_price">Current price</td><td>1-day</td></tr>';
 
 
     $("table").append(html_to_append);
@@ -25,11 +25,10 @@ $(document).ready(function () {
     $(this).closest('tr').remove()
 
 
-    // ticker = document.getElementById('"' + ticker + '"').html();
-
-    // $('"#" + ticker + "_row"').remove();
-    // ticker_array.shift(ticker)
-    // console.log(ticker_array)
+    ticker = $(this).parent().next().html();
+    var index = ticker_array.indexOf(ticker);
+    ticker_array.splice(index, 1)
+    console.log(ticker_array)
 
   }); // end remove-button.click
 
